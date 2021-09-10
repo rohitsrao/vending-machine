b62a34ab13593b2c3f917a6e4cb23a925e2454e6
Rohit <rohitsrao@gmail.com>
2021-09-10 15:03:55 +0200

    Refactoring

--- tests/test_root_route.py
+++ tests/test_root_route.py
@@ -2,9 +2,9 @@ import unittest
 
 from vending_machine import app
 
-class TestBasic(unittest.TestCase):
+class TestRootRoute(unittest.TestCase):
 
-    def test_home_route_response_is_application_json(self):
+    def test_root_route_response_is_application_json(self):
         with app.test_client() as client:
             response = client.get('/')
             self.assertEqual(response.content_type, 'application/json')
